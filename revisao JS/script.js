f862b6cf60e4bbabb5011e549c12b2df21e0bfbb
MiltonhBustos <miltonhenrique2005@gmail.com>
2026-05-07 11:38:55 -0300

    Add files via upload

--- revisao JS/script.js
+++ revisao JS/script.js
@@ -5,7 +5,30 @@ function addTask(){
   const textInput = taskImput.value.trim();
   if(taskImput.value.trim() !== ''){
     const newTask = document.createElement('li');
-    newTask.innerHTML = `<spam> ${textInput} </spam>`;
+    
+    newTask.innerHTML = `<span>${textInput}</span>
+    
+      <button onclick="editar()">Editar</button>
+      <button onclick="remover(this)">Remover</button>
+      <button onclick="concluir(this)">Concluir</button>`
+      
     taskList.appendChild(newTask);
   }
+  
 }
+function editar(){
+    
+  
+  }
+  
+  function remover(button){
+    
+    const itemToRemove = button.parentElement;
+    taskList.removeChild(itemToRemove);
+  }
+  
+  function concluir(button){
+    
+    const itemToComplete = button.parentElement;
+    itemToComplete.classList.toggle = 'completed';
+  }
